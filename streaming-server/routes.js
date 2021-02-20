495d742fa8733a92378ac3fda37bb70e83b97586
hispace <hispace.j@gmail.com>
2021-02-20 17:42:57 +0900

    root path

--- streaming-server/routes.js
+++ streaming-server/routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const ffmpeg = require('fluent-ffmpeg')
 const fs = require('fs')
 
+const ROOT_PATH = 'stream/'
 let ffmpegDict = {}
 
 router.post('/stream/:cctv_id', async function(req, res) {
@@ -13,8 +14,8 @@ router.post('/stream/:cctv_id', async function(req, res) {
     const rtspUrl = `rtsp://${account}:${password}@${rtsp}`
     console.log("Stream Start: " + rtspUrl)
 
-    if (!fs.existsSync(cctvId)) {
-        fs.mkdirSync(cctvId, { recursive: true })
+    if (!fs.existsSync(ROOT_PATH + cctvId)) {
+        fs.mkdirSync(ROOT_PATH + cctvId, { recursive: true })
     }
 
     if (cctvId in ffmpegDict) {
@@ -40,10 +41,10 @@ router.post('/stream/:cctv_id', async function(req, res) {
         ]).on('start', function (commandLine) {
             console.log('FFmpeg with command: ' + commandLine)
         }).on('codecData', function (data) {
-            res.status(200).send({ id : cctvId, url : `/${cctvId}/output.m3u8`})
+            res.status(200).send({ id : cctvId, url : `${ROOT_PATH}/${cctvId}.m3u8`})
         }).on('error', function (err) {
             console.log(err)
-        }).saveToFile(`./${cctvId}/output.m3u8`)
+        }).saveToFile(`${ROOT_PATH}/${cctvId}/output.m3u8`)
     
         ffmpegDict[cctvId] = ffmpegId
     } catch (err) {
@@ -60,7 +61,7 @@ router.delete('/stream/:cctv_id', async function(req, res) {
         if (cctvId in ffmpegDict) {
             const killProcess = ffmpegDict[cctvId]
             killProcess.kill()
-            fs.rmdirSync(cctvId, { recursive: true })
+            fs.rmdirSync(ROOT_PATH + cctvId, { recursive: true })
             delete ffmpegDict[cctvId]
         }
         return res.status(200).send()
